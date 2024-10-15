d4dafce7db78f14a1d6dbed91b218891902283c6
Kirill D <krldrn@icloud.com>
2024-10-15 20:46:13 +0200

    Создание проекта

---
+++ Tracker/Data/Tracker.swift
@@ -0,0 +1,16 @@
+//
+//  Tracker.swift
+//  Tracker
+//
+//  Created by Кирилл Дробин on 14.10.2024.
+//
+
+import UIKit
+
+struct Tracker {
+    let id: Int
+    let trackerName: String
+    let trackerColor: CGColor
+    let trackerEmoji: String
+    let trackerDate: Date
+}
